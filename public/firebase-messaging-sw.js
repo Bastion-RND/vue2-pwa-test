ff679ebd4a9e03a038a66a297accb6be5d66e4bc
Dmitriy Ushkalov <dmitriyushkalov@gmail.com>
2021-12-09 00:23:00 +0300

    Добавил Firebase на фронт

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-undef */
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
-importScripts('https://cdnjs.cloudflare.com/ajax/libs/localforage/1.10.0/localforage.min.js');
 
 const FIREBASE_CONFIG = {
   apiKey: 'AIzaSyBNv1w9uWVOwpw85FVpzmOS-6CAOiTLPww',
@@ -14,36 +13,18 @@ const FIREBASE_CONFIG = {
   vapidKey: 'BMIQjw6WSG2sa5PjKRPQKACxgeEo_ci1_A8-4fdzUWmK5RwNwPOpRuWjlTGrqbUR1_Nh19oZZzujB-fSm0QkN1o',
 };
 
-const INDEXED_DB = {
-  name: 'teplocom',
-  storeName: 'push',
-  description: 'Хранилище PUSH настроек',
-};
-
-const store = localforage.createInstance(INDEXED_DB);
-
 firebase.initializeApp(FIREBASE_CONFIG);
 
 const messaging = firebase.messaging();
 
-messaging.getToken({ vapidKey: FIREBASE_CONFIG.vapidKey }).then((currentToken) => {
-  if (currentToken) {
-    store.setItem('fcmToken', currentToken);
-  } else {
-    // Show permission request UI
-    console.log('No registration token available. Request permission to generate one.');
-  }
-}).catch((err) => {
-  console.log('An error occurred while retrieving token. ', err);
-});
-
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
+
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/img/icons/144x144.png',
+    body: payload.notification.body,
+    icon: '/img/icons/192x192.png',
+    bage: '/img/icons/96x96.png',
   };
 
   // eslint-disable-next-line no-restricted-globals
